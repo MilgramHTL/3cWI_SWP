1549da3b951fdcb5acc99bfd4373b49f50b73f5a
MilgramHTL <lukas.wagner@student.htldornbirn.at>
2023-09-19 09:15:36 +0200

    first_ex_done

--- src/at/milgram/exercises/IF_Ex_1.java
+++ src/at/milgram/exercises/IF_Ex_1.java
@@ -2,21 +2,23 @@ package at.milgram.exercises;
 
 import java.util.Random;
 
-public class ControlStructurs3 {
+public class IF_Ex_1 {
     public static void main(String[] args) {
-        // Erstelle eine Zufallszahl zwischen 0 und 100
         Random random = new Random();
         int randomNumber = random.nextInt(100);
 
-        // Gib die Zufallszahl aus
         System.out.println(randomNumber);
-        // Wenn die Zahl kleiner ist als 20  gib aus "Mini"
-        if  {
 
+        if(randomNumber < 20) {
+            System.out.println("Mini");
+        } else if (randomNumber > 20 && randomNumber < 50) {
+            System.out.println("Medium");
+        } else if (50 < randomNumber) {
+            System.out.println("Large");
         }
+        // Wenn die Zahl kleiner ist als 20  gib aus "Mini"
         // Wenn die Zahl zw. 20 und 50 ist gib aus "Medium"
         // Wenn die Zahl größer als 50 ist gib aus "Large"
-
 
     }
 }
