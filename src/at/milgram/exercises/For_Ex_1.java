8fafbaf7ec2b3c7e24bca65057fec96d9b6cd2f8
MilgramHTL <lukas.wagner@student.htldornbirn.at>
2023-09-22 08:35:28 +0200

    For_Ex_1_done

---
+++ src/at/milgram/exercises/For_Ex_1.java
@@ -0,0 +1,11 @@
+package at.milgram.exercises;
+
+public class For_Ex_1 {
+    public static void main(String[] args) {
+        int l = 0;
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(i+l);
+            l = l + i;
+        }
+    }
+}
