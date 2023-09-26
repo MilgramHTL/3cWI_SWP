7ae8f728aa3f2e35ddefe2afa96e6b7195fdf13a
MilgramHTL <lukas.wagner@student.htldornbirn.at>
2023-09-26 09:31:23 +0200

    Changend_Switch_bound_length

--- src/at/milgram/exercises/Switch_Ex_1.java
+++ src/at/milgram/exercises/Switch_Ex_1.java
@@ -6,7 +6,7 @@ public class Switch_Ex_1 {
     public static void main(String[] args) {
         // Erstelle eine Zufallszahl zwischen 5 und 10
         Random random = new Random();
-        int randomNumber = random.nextInt(5,10);
+        int randomNumber = random.nextInt(5,11);
         // Wenn der Wert 10 ist gib aus Ten
         switch (randomNumber) {
             case 5:
