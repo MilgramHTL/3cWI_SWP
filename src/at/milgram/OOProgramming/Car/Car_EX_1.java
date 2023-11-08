131b94905a93731168d6484551029b0b38cfc2f3
MilgramHTL <lukas.wagner@student.htldornbirn.at>
2023-11-08 09:32:05 +0100

    Started_OO_Programming

---
+++ src/at/milgram/OOProgramming/Car/Car_EX_1.java
@@ -0,0 +1,10 @@
+package at.milgram.OOProgramming.Car;
+
+public class Car_EX_1 {
+    public static void main(String[] args) {
+        String car1Color = "red";
+        double car1Fuel = 73.5;
+        double car1FuelConsumption = 2.3;
+        String car1SerialNumber = "R8";
+    }
+}
