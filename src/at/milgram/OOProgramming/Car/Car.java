96f36f5640451b4cb3fd03103b4bba4245e36824
MilgramHTL <lukas.wagner@student.htldornbirn.at>
2023-11-22 09:07:49 +0100

    Changed_Car_Done_with_Ex_4_and_5

--- src/at/milgram/OOProgramming/Car/Car.java
+++ src/at/milgram/OOProgramming/Car/Car.java
@@ -12,37 +12,55 @@ public class Car {
     public double fuelUsed;
     public Scanner scanner;
     public int amountOfRepetitions;
+    public double fuelCheck;
+    public Car(String color, double fuel, double fuelConsumption, String serialNumber) {
+        this.color = color;
+        this.fuel = fuel;
+        this.fuelConsumption = fuelConsumption;
+        this.serialNumber = serialNumber;
+        this.distance = 0.0;
+    }
 
-    public void driveCar(String color, double fuel, double fuelConsumption, String serialNumber) {
-        System.out.println("Das Auto mit der Farbe " + color + ", Seriennummer " + serialNumber + " fährt.");
+
+    public void driveCar() {
+        System.out.println("Das Auto mit der Farbe " + this.color + ", Seriennummer " + this.serialNumber + " fährt.");
 
         this.distance = 90.0;
-        this.fuelUsed = this.distance / fuelConsumption;
-        this.fuel -= fuelUsed;
+        this.fuelUsed = this.distance / this.fuelConsumption;
+        this.fuel -= this.fuelUsed;
 
         System.out.println("Treibstoffverbrauch: " + this.fuelUsed + " Liter");
         System.out.println("Verbleibender Kraftstoff: " + this.fuel + " Liter");
     }
     public void turboBoost(){
-        if (this.distance/10 < this.fuel) {
-            this.fuel -= 4;
-            System.out.println(this.fuel);
-            System.out.println("SuperBoostMode");
+        double boostFuel = 4;
+        if (this.fuel >= boostFuel) {
+            this.fuel -= boostFuel;
+            System.out.println("SuperBoost-Modus");
+            System.out.println("Treibstoff nach Benutzung des SuperBoost-Modus: " + this.fuel + " Liter");
         } else {
-            System.out.println("Not enough fuel to go to Superboost");
+            System.out.println("Not enough fuel to use Superboost");
         }
     }
     public void honk(){
         System.out.println("Anzahl der Huper: ");
         this.scanner = new Scanner(System.in);
         this.amountOfRepetitions = Integer.parseInt(this.scanner.next());
-        for(int i = 0; i <= this.amountOfRepetitions; i++) {
+        for(int i = 0; i < this.amountOfRepetitions; i++) {
             System.out.println("Tuuuuuuut");
         }
     }
     public void getRemainingRange(){
-        this.remainingDistance = this.distance - this.fuelUsed;
+        this.remainingDistance = this.fuel/this.fuelConsumption;
         System.out.println("Das Fahrzeug könnte noch: " + this.remainingDistance + " fahren.");
+    }
+    public void checkFuel(){
+        this.fuelCheck = this.fuel;
+        if(this.fuelCheck > 0){
+            System.out.println("Das Auto hat noch Treibsstoff.");
+        }else {
+            System.out.println("Das Auto hat keinen Treibstoff mehr.");
+        }
     }
     public void brakeCar() {
         System.out.println("Das Auto bremst.");
--- src/at/milgram/OOProgramming/Car/Car_Execute.java
+++ src/at/milgram/OOProgramming/Car/Car_Execute.java
@@ -2,28 +2,23 @@ package at.milgram.OOProgramming.Car;
 
 public class Car_Execute {
     public static void main(String[] args) {
-        Car c1 = new Car();
-        c1.color = "red";
-        c1.fuel = 69;
-        c1.fuelConsumption = 3;
-        c1.serialNumber = "R8";
+        Car c1 = new Car("red", 69, 3, "R8");
+        Car c2 = new Car("blue", 64, 2, "A5");
 
-        Car c2 = new Car();
-        c2.color = "blue";
-        c2.fuel = 64;
-        c2.fuelConsumption = 2;
-        c2.serialNumber = "A5";
-
-        c1.driveCar(c1.color, c1.fuel, c1.fuelConsumption, c1.serialNumber);
+        c1.driveCar();
         c1.turboBoost();
         c1.honk();
         c1.brakeCar();
         c1.getRemainingRange();
+        c1.checkFuel();
 
-        c2.driveCar(c2.color, c2.fuel, c2.fuelConsumption, c2.serialNumber);
+        System.out.println(" ");
+
+        c2.driveCar();
         c2.turboBoost();
         c2.honk();
         c2.brakeCar();
         c2.getRemainingRange();
+        c2.checkFuel();
     }
 }
