e8615acc8e930a8d6c07caebd5ede5ce39a736c7
MilgramHTL <lukas.wagner@student.htldornbirn.at>
2023-12-18 13:56:27 +0100

    Unexpected_Error_created_tank_and_engine

--- src/at/milgram/OOProgramming/Car/Car_Execute.java
+++ src/at/milgram/OOProgramming/Car/Car_Execute.java
@@ -2,19 +2,25 @@ package at.milgram.OOProgramming.Car;
 
 public class Car_Execute {
     public static void main(String[] args) {
-        Tank tank = new Tank(200, 2, "Titan");
-        Engine engine = new Engine(100, Engine.TYPE.Diesel);
-        Car c1 = new Car(engine,"red", 69,  "R8");
-        Car c2 = new Car(engine,"blue", 64,  "A5");
-        Car c3 = new Car(engine,"purple", 99,  "SS9");
+        Tank tank1 = new Tank(200, 2, "Titan");
+        Tank tank2 = new Tank(300, 3, "Wolfram");
+        Tank tank3 = new Tank(400, 4, "Eisen");
 
-        tank.getRemainingRange();
-        engine.drive(17);
-        engine.driveCar(3000);
+        Engine engine1 = new Engine(100, Engine.TYPE.Diesel);
+        Engine engine2 = new Engine(100, Engine.TYPE.Diesel);
+        Engine engine3 = new Engine(100, Engine.TYPE.Diesel);
+
+        Car c1 = new Car(engine1, tank1, "red", 69,  "R8");
+        Car c2 = new Car(engine2, tank2, "blue", 64,  "A5");
+        Car c3 = new Car(engine3, tank3, "purple", 99,  "SS9");
+
+        tank1.getRemainingRange();
+        engine1.drive(17);
+        engine1.driveCar(3000);
         c1.turboBoost();
         c1.honk();
         c1.brakeCar();
-        // c1.getRemainingRange();
+        c1.getRemainingRange();
         c1.checkFuel();
 
         System.out.print("Die PS vom Auto 1 beträgt: ");
@@ -26,14 +32,12 @@ public class Car_Execute {
 
         System.out.println(" ");
 
-
         c2.turboBoost();
         c2.honk();
         c2.brakeCar();
         c2.checkFuel();
 
         System.out.println(" ");
-
 
         c3.turboBoost();
         c3.honk();
